d2e8d74e9dfa1fa1fa7c54ac528300443fb7e565
Yuliya513 <83278354+Yuliya513@users.noreply.github.com>
2021-07-02 11:58:33 +0300

    text

---
+++ src/com/company/Text.java
@@ -0,0 +1,19 @@
+package com.company;
+# include <thread>
+# include <future>
+
+public class Text {
+
+    public static void main(String[] args) {
+	void asyncText(){
+	    System.out.println("<<a,b,c << end 1")
+        }
+        int main (){
+	    System.out.println("Main Thread");
+	    this_thread:: get_id()<<end1;
+	    future <void>fn=async(launch:: any, asyncText);
+            System.out.println("launch: any asyncText");
+
+        }
+    }
+}
